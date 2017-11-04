c1f115994f9e120aca8958e33daeccd40a41b696
Polaris <fztfztfztfzt@gmail.com>
2017-11-04 22:53:28 +0800

    spider update

--- spider/spider.py
+++ spider/spider.py
@@ -1,15 +1,17 @@
 import requests
 import re
+import json
 
 keywords = []
 with file("keywords.txt") as f:
     keywords = f.readlines()
 keywords = [d.strip() for d in keywords]
 
+
 while(True):
-    temp = requests.get('http://www.doutula.com/so/init')
-    temp = temp.json()
-    temp2 = [d['keyword'] for d in temp['hot']]
+    temp = requests.get('http://www.doutula.com/so/init').json()
+    print temp['hot'].values()
+    temp2 = [d['keyword'] for d in temp['hot'].values()]
     for d in temp['realtime']:
         if not d['keyword'] in temp2:
             temp2.append(d['keyword'])
