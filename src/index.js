157058be42b342a3e96a11644612b36d67bbb66d
Nabufaces <598824163@qq.com>
2017-11-04 14:26:41 +0800

    front commit

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ var vm = new Vue({
     el: '#app',
     data: {
         EMOJI_MAP: jEmoji.EMOJI_MAP,
-        showSelectOption: false
+        chosenEmoji: ''
     },
     methods: {
-        showSelect: function () {
-            this.$data.showSelectOption = true;
-        },
-        choseEmoji: function (params) {
-            axios.get('/public/getImg', {
-                desc: params[1]
-            }).then((response) => {
-                console.log(response)
-            }).catch((response) => {
-                console.log(response)
-            })
+        choseEmoji: function (chosenEmoji, desc) {
+            this.$data.chosenEmoji = chosenEmoji;
+            axios.get(`/public/getImg?desc=${desc}`)
+                .then((response) => {
+                    console.log(response)
+                }).catch((response) => {
+                    console.log(response)
+                })
         }
     }
 });
