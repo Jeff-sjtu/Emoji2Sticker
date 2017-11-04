564de58ed5038ec26a5c47f56b03c32e700830ad
Jeff-sjtu <jeff.lee.sjtu@gmail.com>
2017-11-04 23:23:14 +0800

    spider

--- spider/spider.py
+++ spider/spider.py
@@ -10,14 +10,24 @@ keywords = [d.strip() for d in keywords]
 
 while(True):
     temp = requests.get('http://www.doutula.com/so/init').json()
-    print temp['hot'].values()
-    temp2 = [d['keyword'] for d in temp['hot'].values()]
-    for d in temp['realtime']:
-        if not d['keyword'] in temp2:
-            temp2.append(d['keyword'])
-    for d in temp2:
+    #print temp['hot'].values()
+    temp2 = [d for d in temp['hot'].values()]
+    keywordlist = []
+    if 'keyword' in temp2[0].keys():
+        for item in temp2:
+            keywordlist.append(item['keyword'])
+    else:
+        for item in temp2:
+            key = item.keys()[0]
+            keywordlist.append(item[key]['keyword'])
+
+    for d in keywordlist:
         if not d in keywords:
             keywords.append(d)
             temp = requests.get('http://www.doutula.com/search?keyword='+d)
-            print temp.text
+            #print temp.text
+            reg = re.compile(r'data-original="(.*)" style.*\n.*none">(.*)<')
+            regMatch = reg.match(temp.text)
+            for item in regMatch:
+                print(type(item))
             raw_input()
