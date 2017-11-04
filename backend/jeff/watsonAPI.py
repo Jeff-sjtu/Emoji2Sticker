03b733f46db5fb216ffde50099e0feec4925b2b5
Jeff-sjtu <jeff.lee.sjtu@gmail.com>
2017-11-04 21:06:19 +0800

    update watsonAPI

--- backend/jeff/watsonAPI.py
+++ backend/jeff/watsonAPI.py
@@ -5,5 +5,14 @@ natural_language_classifier = NaturalLanguageClassifierV1(
   username='726ac318-6015-4666-b38e-df099c571619',
   password='7N3j4DZZSDPZ')
 
-classes = natural_language_classifier.classify('ebd2f7x230-nlc-71076', 'How hot will it be today?')
-print(json.dumps(classes, indent=2))
+#classes = natural_language_classifier.classify('ebd2f7x230-nlc-71076', 'How hot will it be today?')
+#print(json.dumps(classes, indent=2))
+
+def getClasses(tmp):
+    classes = natural_language_classifier.classify('1e0d8ex232-nlc-28667', tmp)
+    return classes
+
+def 
+
+if __name__ == '__main__':
+    getClasses("It's hot today.")
