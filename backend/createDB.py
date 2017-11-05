7b2b5f42e4f495283d0faefdff64c8f0093406f9
Jeff-sjtu <jeff.lee.sjtu@gmail.com>
2017-11-05 10:23:35 +0800

    update

--- backend/createDB.py
+++ backend/createDB.py
@@ -22,20 +22,26 @@ def main():
     #print "==================="
     #print getI(cur,1)
     #print "==================="
-    f = open('../frog/pictures/name.txt','r')
+    f = open('../frog/static/dog.txt','r')
     info = f.readlines()
     cnt = 0
     for item in info:
-        imgName, dcrp = item.split(',')
+        aline = item.split(',')
+        imgName = aline[0]
+        dcrp = aline[1:]
         imgName = imgName.strip()
-        dcrp = dcrp.strip()
+        #dcrp = dcrp.strip()
         #img = [imgName, dcrp]
-        tag_info = getClasses(dcrp)
+        tag_info = getClasses("")
         tag_info['discription'] = dcrp
         tag_info['imgName'] = imgName
+        tag_info['classes'] = []
+        for tag in dcrp:
+            tag_info['classes'].append({u'class_name': tag, u'confidence': 1})
         #with open('./pictures/%s' %imgName, 'rb') as imgfile:
         base64_data = base64.b64encode(imgName)
-        insert(cur,base64_data,tag_info)
+        print(tag_info)
+        #insert(cur,base64_data,tag_info)
         #img.append(tag_info)
             #print(getALL(cur))
         cnt += 1
